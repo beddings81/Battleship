aaad5a224088e305eed9b9657a429968553740cf
Anhnhi Tran <anhtran811@gmail.com>
2022-10-17 19:14:56 -0600

    Got hit test to pass

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,13 +1,22 @@
 class Ship
-  attr_reader :name, :length
+  attr_reader :name, :length, :health
 
   def initialize(name, length)
     @name = name
     @length = length
+    @health = length
   end
 
-  def health
-    @length
+  def sunk?
+    if @health == 0
+      return true
+    else
+      return false
+    end
+  end
+
+  def hit
+    @health -= 1
   end
 
 end
