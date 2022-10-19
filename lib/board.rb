9df58c8ddd2a533d3234e8abd25f1dcc834fa5e4
Anhnhi Tran <anhtran811@gmail.com>
2022-10-19 11:48:04 -0600

    Got cells to pass

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,25 @@
 class Board
   def initialize
   end
+
+  def cells
+    cells = {
+      "A1" => cell = Cell.new("A1"),
+      "A2" => cell = Cell.new("A2"),
+      "A3" => cell = Cell.new("A3"),
+      "A4" => cell = Cell.new("A4"),
+      "B1" => cell = Cell.new("B1"),
+      "B2" => cell = Cell.new("B2"),
+      "B3" => cell = Cell.new("B3"),
+      "B4" => cell = Cell.new("B4"),
+      "C1" => cell = Cell.new("C1"),
+      "C2" => cell = Cell.new("C2"),
+      "C3" => cell = Cell.new("C3"),
+      "C4" => cell = Cell.new("C4"),
+      "D1" => cell = Cell.new("D1"),
+      "D2" => cell = Cell.new("D2"),
+      "D3" => cell = Cell.new("D3"),
+      "D4" => cell = Cell.new("D4")
+    }
+  end
 end
