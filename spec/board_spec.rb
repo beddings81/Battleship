ae5455eae7f116b74820f2f9806611010d057a14
Anhnhi Tran <anhtran811@gmail.com>
2022-10-19 15:24:55 -0600

    Add validate board test to board spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -14,4 +14,15 @@ RSpec.describe Board do
 
     expect(board.cells).to be_a(Hash)
   end
+
+  it 'can validate a coordinate' do
+    board = Board.new
+
+    expect(board.cells).to be_a(Hash)
+    expect(board.valid_coordinate?("A1")).to eq(true)
+    expect(board.valid_coordinate?("D4")).to eq(true)
+    expect(board.valid_coordinate?("A5")).to eq(false)
+    expect(board.valid_coordinate?("E1")).to eq(false)
+    expect(board.valid_coordinate?("A22")).to eq(false)
+  end
 end
