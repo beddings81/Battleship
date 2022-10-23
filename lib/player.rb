d5ef8c8cbdab3d2e932fc587996550399df729a1
Braxton Eddings <beddings81@gmail.com>
2022-10-23 15:10:19 -0500

    Add player class file

---
+++ lib/player.rb
@@ -0,0 +1,25 @@
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require './lib/game'
+
+class Player
+  attr_reader :cruiser, :submarine, :board
+
+  def initialize
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
+    @board = Board.new
+  end
+
+  def place_cruiser
+    puts "Enter the squares for the Cruiser (3 spaces):"
+    cruiser_coordinate = gets.chomp.upcase.split
+    while @board.valid_placement?(cruiser, cruiser_coordinate) == false
+      puts "Those are invalid coordinates. Please try again!"
+      cruiser_coordinate = gets.chomp.upcase.split
+    end
+    @board.place(cruiser, cruiser_coordinate)
+    puts @board.render(true)
+  end
+end
