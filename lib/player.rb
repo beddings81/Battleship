806b51c8021b33bbe55ccd10a655622dfe24196a
Anhnhi Tran <anhtran811@gmail.com>
2022-10-24 15:15:00 -0600

    Hit and miss working for shots method

--- lib/player.rb
+++ lib/player.rb
@@ -10,7 +10,6 @@ class Player
     @cruiser = Ship.new("Cruiser", 3)
     @submarine = Ship.new("Submarine", 2)
     @board = Board.new
-    @board_cpu = Board.new
   end
 
   def place_cruiser
@@ -35,73 +34,51 @@ class Player
     puts @board.render(true)
   end
 
+  def computer_board(computer_board)
+    @board_cpu = computer_board
+  end
+
   def computer_place_cruiser
     coordinates_array = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4" ]
     cruiser_coordinate = [] 
-      while @board_cpu.valid_placement?(cruiser, cruiser_coordinate) == false
+      while @board.valid_placement?(cruiser, cruiser_coordinate) == false
         cruiser_coordinate = coordinates_array.sample(3)
       end
-    @board_cpu.place(cruiser, cruiser_coordinate)
-    puts @board_cpu.render(true)
+    @board.place(cruiser, cruiser_coordinate)
+    puts @board.render(true)
   end
 
   def computer_place_submarine
     coordinates_array = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4" ]
     submarine_coordinate = coordinates_array.sample(2)
-      while @board_cpu.valid_placement?(submarine, submarine_coordinate) == false
+      while @board.valid_placement?(submarine, submarine_coordinate) == false
         submarine_coordinate = coordinates_array.sample(2)
       end
-    @board_cpu.place(submarine, submarine_coordinate)
-    puts @board_cpu.render(true)
+    @board.place(submarine, submarine_coordinate)
+    puts @board.render(true)
   end
 
   def shots
     puts "Enter the coordinate for your shot:"
     player_shot = gets.chomp.upcase
     # if not valid coordinate
-
     while @board.valid_coordinate?(player_shot) == false
       puts "This is not a valid coordinate, please enter a valid coordinate:"
       player_shot = gets.chomp.upcase
-      if @board.valid_coordinate?(player_shot) == true
-        @board_cpu.cells[player_shot].fire_upon
-        puts @board_cpu.render(true)
-        if @board_cpu.cells[player_shot].fired_upon? == true && @board_cpu.cells[player_shot].empty? == true
-          puts "Your shot on #{player_shot} was a miss"
-        # elsif
-          #feeedback for X
-          #fired_upon? == true && @ship.sunk? == true && empty? == false
-        # else
-         #feedback for H
-         #fired_upon? == true && empty? == false
-        end
+    end
+    if @board.valid_coordinate?(player_shot) == true
+      @board_cpu.cells[player_shot].fire_upon
+      puts @board_cpu.render
+      if @board_cpu.cells[player_shot].fired_upon? == true && @board_cpu.cells[player_shot].empty? == true
+        puts "Your shot on #{player_shot} was miss."
+      else @board_cpu.cells[player_shot].fired_upon? == true && @board_cpu.cells[player_shot].empty? == false
+        puts "Your shot on #{player_shot} was a hit!"
+      # else @board_cpu.cells[player_shot].fired_upon? == true && @board_cpu.cells[player_shot].empty? == false && @board_cpu.cells[player_shot].ship.sunk? == true
+      #   puts "Your shot on #{player_shot} sunk my battleship!"
       end
     end
   end
 end
-        # def place_submarine
-        #   puts "Enter the squares for the Submarine (2 spaces):"
-        #   submarine_coordinate = gets.chomp.upcase.split
-        #   while @board.valid_placement?(submarine, submarine_coordinate) == false
-        #     puts "Those are invalid coordinates. Please try again!"
-        #     submarine_coordinate = gets.chomp.upcase.split
-        #   end
-        #   @board.place(submarine, submarine_coordinate)
-        #   puts @board.render(true)
-        # end
-      
-    # # if it was hit
-    # puts "Your shot on A4 was a miss."
-    # #if it was a miss
-    # puts "My shot on C1 was a miss."
-
-# def valid_coordinate?(coordinate)
-#   coordinate_names = @cells.keys
-#   if @cells.keys.include?(coordinate)
-#     return true
-#   else
-#     return false
-#   end
 
 # def place_submarine
 #   puts "Enter the squares for the Submarine (2 spaces):"
